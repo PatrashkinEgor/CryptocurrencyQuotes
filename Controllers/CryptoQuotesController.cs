315fb6545062b8de3f4a347c5549ffabdbf2fe19
Patrashkin Egor <patrashkin@labsys.ru>
2021-07-12 09:07:50 +0500

    Добавьте файлы проекта.

---
+++ Controllers/CryptoQuotesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CryptocurrencyQuotes.Controllers
+{
+    public class CryptoQuotesController : Controller
+    {
+        // GET: CryptoQuotes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CryptocurrencyQuotes.MvcApplication" Language="C#" %>
