70ebdace865afa206887f4f68023827b02ba15c1
Егор Патрашкин <eapat@mail.ru>
2021-07-15 00:17:25 +0500

    Redirect to Home/About

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -392,7 +392,7 @@ namespace CryptocurrencyQuotes.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("About", "Home");
         }
 
         //
@@ -449,7 +449,7 @@ namespace CryptocurrencyQuotes.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("About", "Home");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
